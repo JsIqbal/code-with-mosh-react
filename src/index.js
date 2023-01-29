8fc0d56994771c372b864d6c6b27c7b3a8e9b25f
Iqbal Hossain <iqbal.hossain@ada-asia.com>
2023-01-29 17:48:00 +0600

    Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const address = {
+    name: "Iqbal",
+    city: "Dhaka",
+    sector: "Sector-10",
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    personal: {
+        number: "xxx",
+    },
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const name = address.name; // repetive object
+// const city = address.city;
+// const sector = address.sector;
+
+const { name, city, sector } = address; // non repitive object
+
+const { city: ct } = address;
+
+const {
+    personal: { number: contact },
+} = address;
+
+console.log(contact);
